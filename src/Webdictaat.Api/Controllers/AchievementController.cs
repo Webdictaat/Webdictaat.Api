cade04a39395ca079c931d97ac7578a0e1de04ac
Michel du Croix <mlcroix@avans.nl>
2017-05-07 12:00:02 +0200

    achievements basic

---
+++ src/Webdictaat.Api/Controllers/AchievementController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Webdictaat.Api.Services;
+using Webdictaat.CMS.Models;
+using Webdictaat.CMS.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Webdictaat.Api.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Webdictaat.Domain.User;
+using Webdictaat.Domain;
+using Webdictaat.Api.Models;
+
+namespace Webdictaat.Api.Controllers
+{
+    [Route("api/dictaten/{dictaatName}/[controller]")]
+    public class AchievementController : Controller
+    {
+        private IAchievementRepository _achievementRepo;
+        private UserManager<ApplicationUser> _userManager;
+
+        [HttpGet]
+        [Authorize]
+        public AchievementVM Get(string dictaatName)
+        {
+            string userId = _userManager.GetUserId(HttpContext.User);
+            AchievementVM result = _achievementRepo.GetAllAchievements(dictaatName, userId);
+            return result;
+        }
+
+        [HttpGet("{achievementId}")]
+        [Authorize]
+        public AchievementVM Get(int achievementId, string dictaatName)
+        {
+            string userId = _userManager.GetUserId(HttpContext.User);
+            AchievementVM result = _achievementRepo.GetAchievement(achievementId, dictaatName, userId);
+            return result;
+        }
+    }
+}
---
+++ src/Webdictaat.Api/Models/AchievementRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webdictaat.Api.ViewModels;
+using Webdictaat.Domain;
+
+namespace Webdictaat.Api.Models
+{
+    public interface IAchievementRepository
+    {
+        AchievementVM GetAchievement(int achievementId, string dictaatName, string userId);
+        AchievementVM GetAllAchievements(string dictaatName, string userId);
+        void CheckAchievement(string dictaatName, int AchievementId, Boolean check, string userId);
+        void AddAchievement(string dictaatName, Achievement achieve);
+        void DeleteAchievement(string dictaatName, int achievementId);
+        void UpdateAchievement(string dictaatName, int achievementId, Achievement achieve);
+    }
+
+    public class AchievementRepository : IAchievementRepository
+    {
+        public void AddAchievement(string dictaatName, Achievement achieve)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckAchievement(string dictaatName, int AchievementId, bool check, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAchievement(string dictaatName, int achievementId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AchievementVM GetAchievement(int achievementId, string dictaatName, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AchievementVM GetAllAchievements(string dictaatName, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAchievement(string dictaatName, int achievementId, Achievement achieve)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
