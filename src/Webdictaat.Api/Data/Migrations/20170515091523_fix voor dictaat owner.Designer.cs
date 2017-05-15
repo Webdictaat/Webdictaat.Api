49eafc70e323f5dce094806e8014a97f8e8540b1
Michel du Croix <mlcroix@avans.nl>
2017-05-15 11:18:58 +0200

    fix voor dictaat owner migration

--- src/Webdictaat.Api/Data/Migrations/20170515091523_fix voor dictaat owner.Designer.cs
+++ src/Webdictaat.Api/Data/Migrations/20170515091523_fix voor dictaat owner.Designer.cs
@@ -8,9 +8,10 @@ using Webdictaat.Data;
 namespace Webdictaat.Api.Migrations
 {
     [DbContext(typeof(WebdictaatContext))]
-    partial class WebdictaatContextModelSnapshot : ModelSnapshot
+    [Migration("20170515091523_fix voor dictaat owner")]
+    partial class fixvoordictaatowner
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
