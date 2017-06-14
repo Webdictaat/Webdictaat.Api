1861350f8a2ef6f37caf531efb0d431ae9f891cd
Michel du Croix <mlcroix@avans.nl>
2017-06-14 14:30:08 +0200

    userachievement db class

--- src/Webdictaat.Domain/GameElements/Achievement.cs
+++ src/Webdictaat.Domain/GameElements/Achievement.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using Webdictaat.Domain.User;
 
 namespace Webdictaat.Domain
 {
@@ -29,6 +30,25 @@ namespace Webdictaat.Domain
         [ForeignKey("AchievementId")]
 
         public virtual Achievement Achievement { get; set; }
+    }
+
+    public class UserAchievement
+    {
+        [ForeignKey("UserId")]
+        public virtual ApplicationUser User { get; set; }
+
+        public string UserId { get; set; }
+
+        public int AchievementId { get; set; }
+
+        [ForeignKey("AchievementId")]
+
+        public virtual Achievement Achievement { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public bool Completed { get; set; }
+
     }
     
     public class Achievement
