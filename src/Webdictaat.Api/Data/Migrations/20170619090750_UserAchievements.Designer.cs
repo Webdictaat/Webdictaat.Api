720ab078f2c07200fbe8a24c1cd6d71c0222ac80
Michel du Croix <mlcroix@avans.nl>
2017-06-19 11:54:54 +0200

    userachievements migration

--- src/Webdictaat.Api/Data/Migrations/20170619090750_UserAchievements.Designer.cs
+++ src/Webdictaat.Api/Data/Migrations/20170619090750_UserAchievements.Designer.cs
@@ -8,9 +8,10 @@ using Webdictaat.Data;
 namespace Webdictaat.Api.Migrations
 {
     [DbContext(typeof(WebdictaatContext))]
-    partial class WebdictaatContextModelSnapshot : ModelSnapshot
+    [Migration("20170619090750_UserAchievements")]
+    partial class UserAchievements
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
