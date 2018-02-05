17fe4f9de6e988357c5b1092ca07de5045483212
Stino <linksonder@gmail.com>
2018-02-05 15:52:39 +0100

    page repo 'undo' issue met controller fixes #45

--- src/Webdictaat.Api/Controllers/PagesController.cs
+++ src/Webdictaat.Api/Controllers/PagesController.cs
@@ -57,7 +57,7 @@ namespace Webdictaat.Api.Controllers
             if (!AuthorizeResrouce(dictaatName))    
                 return null;
 
-            return await _pageRepo.GetDictaatPages(dictaatName);
+            return _pageRepo.GetDictaatPages(dictaatName);
         }
 
         /// <summary>
