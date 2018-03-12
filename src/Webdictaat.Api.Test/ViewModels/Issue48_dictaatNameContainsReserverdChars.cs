24772705faed085038ac5652117aa03dee5e3f58
Stino <linksonder@gmail.com>
2018-03-12 11:18:25 +0100

    Backend check for dictaat name fixes #48

---
+++ src/Webdictaat.Api.Test/ViewModels/Issue48_dictaatNameContainsReserverdChars.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Webdictaat.Api.ViewModels;
+using Xunit;
+
+namespace Webdictaat.Api.Test.ViewModels
+{
+    public class Issue48_dictaatNameContainsReserverdChars
+    {
+        [Fact]
+        public void Name_Is_Valid_Test()
+        {
+            //1. arrange
+            var model = new DictaatForm();
+            model.Name = "ABCD1234";
+
+            //2. act
+            var result = Validator.TryValidateObject(model, new ValidationContext(model, null, null), null, true);
+
+            //3. assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Name_Is_Invalid_Test()
+        {
+            //1. arrange
+            var model = new DictaatForm();
+            model.Name = "AB#CD12345";
+
+            //2. act
+            var result = Validator.TryValidateObject(model, new ValidationContext(model, null, null), null, true);
+
+            //3. assert
+            Assert.False(result);
+        }
+        [Fact]
+        public void Name_Is_ToLong_Test()
+        {
+            //1. arrange
+            var model = new DictaatForm();
+            model.Name = "0123456789ABCDEFGHIJX";
+
+            //2. act
+            var result = Validator.TryValidateObject(model, new ValidationContext(model, null, null), null, true);
+
+            //3. assert
+            Assert.False(result);
+        }
+
+    }
+}
--- src/Webdictaat.Api/ViewModels/DictaatForm.cs
+++ src/Webdictaat.Api/ViewModels/DictaatForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,12 @@ namespace Webdictaat.Api.ViewModels
 {
     public class DictaatForm
     {
+        /// <summary>
+        /// Name of the dictaat
+        /// </summary>
+        [Required]
+        [MaxLength(20)]
+        [RegularExpression("^[a-zA-Z0-9_]*$")]
         public string Name { get; set; }
 
         public string Template { get; set; }
